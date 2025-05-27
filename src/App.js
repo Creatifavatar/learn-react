5d437244c2948babb4368fa6e87803de83f52e65
Avatar Créatif <avatarcreatif@gmail.com>
2025-05-27 07:50:59 +0000

    maj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 function App() {
   // state (état, données)
-
+  const [compteur, setCompteur] = useState(1);
   // comportements
 
   // affichage (render)
-  return <h1>Bonjour !</h1>;
+  return (
+    <div>
+      <h1>{compteur}</h1>
+      <button onClick={handleClick}>Incrémente</button>
+    </div>
+  );
 }
 
 export default App;
